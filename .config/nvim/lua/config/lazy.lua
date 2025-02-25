f3e1625275639bb35538fd0c0031e8be202fae37
Shashank Singh <96770927+HoneyBearCodes@users.noreply.github.com>
2025-02-25 22:13:58 +0530

    🌟 Lazyvim (Neovim) configure clangd

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,7 +24,9 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-    { import = "plugins" },
+    { import = "plugins.ui" },
+    { import = "plugins.lsp" },
+    { import = "plugins.extras.lang" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
